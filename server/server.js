22adcdbdb5188dd94bc7036bacdc5de1adaa1c3a
stygmah <stygmah@gmail.com>
2017-05-21 17:48:14 +0200

    Implemented socket

--- server/server.js
+++ server/server.js
@@ -1,13 +1,29 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 var app = express();
 var port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname,'../public');
+var server = http.createServer(app);
+var io = socketIO(server);
 
+const publicPath = path.join(__dirname,'../public');
 app.use(express.static(publicPath));
 
 
-app.listen(port,()=>{
+
+io.on('connection',(socket)=>{
+	console.log('new user connected');
+});
+io.on('disconnect',()=>{
+	console.log('Disconnected to server');
+
+	socket.on('disconnect',()=>{
+			console.log('User disconnect');
+	});
+});
+
+server.listen(port,()=>{
 	console.log('Server running on port ',port);
 });
